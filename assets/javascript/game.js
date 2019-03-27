7ad2176555280b9f386447988e29e4b082c3c116
Amir P <apourrezaei@gmail.com>
2019-03-27 18:22:40 -0400

    Answers - stable 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,9 @@ var done = false;
 var rndChar = Math.floor(Math.random() * 26 + 97);
 // var divAnswer = document.getElementsByClassName("divAnswer");
 var answers = [];
+var lastAnswerIndex;
+var lastAnswer;
+var divAnswerVal = document.getElementById("divAnswerVal");
 
 document.onkeypress = function(event) {
 
@@ -13,17 +16,19 @@ document.onkeypress = function(event) {
     var tempChar = String.fromCharCode(tempKeyCode);
     var used = guesses.indexOf(tempChar);
     var rndCharVal = String.fromCharCode(rndChar);
+    answers.push(rndCharVal);
     
-   
     console.log(answers);
     console.log("rndChar: " + rndChar);
     console.log("tempKeyCode: " + tempKeyCode);
-    var divAnswerVal = document.getElementById("divAnswerVal");
-    divAnswerVal.innerHTML = answers;
+    
+    lastAnswerIndex = answers.length-1;
+    lastAnswer = answers[lastAnswerIndex];
+    divAnswerVal.innerHTML = lastAnswer;
     
     if(tempKeyCode==rndChar) {
         wins++;
-      
+        divAnswerVal.style.display = "block";
         document.getElementById("wins").innerHTML = wins;
         ResetChar();
         ResetGuesses();
